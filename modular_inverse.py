e900e92177a56727e9e68f56e0f239e9a2670de2
Neelam Gurjar <39006317+nsg1999@users.noreply.github.com>
2018-12-12 18:31:40 +0530

    Add files via upload

---
+++ modular_inverse.py
@@ -0,0 +1,20 @@
+#a = 3
+
+#p = 19
+#b = 9
+
+#for i in range(2, p-2):
+#	if( b == (a**i)):
+#		print("ATTACK DONE")
+
+#calculating modular inverse:
+
+a = 3
+c = 7
+
+for i in range(7):
+	if( (a*i)%7 == 1):
+		print(str(i) + "is the inverse of 3 mod 7 ")
+		
+
+
